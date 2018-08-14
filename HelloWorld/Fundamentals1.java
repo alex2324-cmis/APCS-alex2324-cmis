7faea6d29a631e68e798cb4ce1c28c5041dee047
Alex <alex2324@cmis.ac.th>
2018-08-14 09:34:08 +0700

    started on datatype problems

---
+++ HelloWorld/Fundamentals1.java
@@ -0,0 +1,13 @@
+
+public class Fundamentals1
+{
+    public static void main(String args[]){
+        dataTypes();
+    }
+    
+    public static void dataTypes(){
+        System.out.println("Byte");
+        byte a = 100;
+    }
+
+}
