1c8c31107ff14aa3284d2d76708d7114f9e7a53e
Alex <alex2324@cmis.ac.th>
2018-08-15 11:56:56 +0700

    Byte,Short, working on long

--- HelloWorld/Fundamentals1.java
+++ HelloWorld/Fundamentals1.java
@@ -6,8 +6,25 @@ public class Fundamentals1
     }
     
     public static void dataTypes(){
-        System.out.println("Byte");
-        byte a = 100;
+        byte a = 10;
+        short s = 10000;
+        int b = 100000;
+        long c = 100000L;
+        float d = 234.5f;
+        double e = 123.4;
+        boolean f = true;
+        char g = 'A';
+        String h = "Byte data type is an 8-bit signed two's complement integer";
+        String i = "Byte can represent the amount of fingers a person has.";
+        String j = "Short data type is a 16-bit signed two's complement integer";
+        String k = "Short can represent the amount of sleep that I want (in seconds)";
+        String m = "Int data type is a 32-bit signed two's complement integer.";
+        String l = "Int can represent the amount of calories 15 people consumed in their lifetime";
+        String n = "Long data type is a 64-bit signed two's complement integer";
+
+        
+
+
     }
 
 }
