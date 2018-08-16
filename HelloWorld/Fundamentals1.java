57ba1b13238caa68a9813ea890b30f7f93f5abdd
Alex <alex2324@cmis.ac.th>
2018-08-16 14:36:43 +0700

    Tried binary

--- HelloWorld/Fundamentals1.java
+++ HelloWorld/Fundamentals1.java
@@ -6,7 +6,7 @@ public class Fundamentals1
     }
     
     public static void dataTypes(){
-        byte a = 10;
+        byte a = 0b00010100;
         short s = 10000;
         int b = 100000;
         long c = 100000L;
@@ -14,6 +14,7 @@ public class Fundamentals1
         double e = 123.4;
         boolean f = true;
         char g = 'A';
+        System.out.println(a);
         String h = "Byte data type is an 8-bit signed two's complement integer";
         String i = "Byte can represent the amount of fingers a person has.";
         String j = "Short data type is a 16-bit signed two's complement integer";
