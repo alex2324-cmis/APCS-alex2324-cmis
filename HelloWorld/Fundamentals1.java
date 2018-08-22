e933b6fccedca91307e192774c2ef48e352c0f3a
Alex <alex2324@cmis.ac.th>
2018-08-22 14:34:42 +0700

    Almost Done with number 1

--- HelloWorld/Fundamentals1.java
+++ HelloWorld/Fundamentals1.java
@@ -14,17 +14,27 @@ public class Fundamentals1
         double e = 123.4;
         boolean f = true;
         char g = 'A';
-        System.out.println(a);
         String h = "Byte data type is an 8-bit signed two's complement integer";
         String i = "Byte can represent the amount of fingers a person has.";
+        String ae = "Byte can represent the amount of pencils a person has";
         String j = "Short data type is a 16-bit signed two's complement integer";
         String k = "Short can represent the amount of sleep that I want (in seconds)";
+        String kd = "Short can represent 170 weeks in hours";
         String m = "Int data type is a 32-bit signed two's complement integer.";
         String l = "Int can represent the amount of calories 15 people consumed in their lifetime";
+        String lm = "Int can represent the amount of seconds a person can live";
         String n = "Long data type is a 64-bit signed two's complement integer";
         String o = "Long data can represent a light year in meters";
         String p = "Float data type is a single-precision 32-bit IEEE 754 floating point";
         String q = "Float can be represented as 1 million light year in cenimeters";
+        String r = "Double data type is a double-precision 64-bit IEEE 754 floating point";
+        String t = "Double can be represented as one quintillion Jeff Bezos's net worth";
+        String u = "Boolean data type represents one bit of information";
+        String v = "Boolean can be represented as am I a boy?";
+        String w = "Char data type is a single 16-bit Unicode character";
+        String x = "Char can represent the most expensive keyboard in Cambodian riel";
+        
+
       
 
         
