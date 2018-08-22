896108021d7095e3e9ebbb73e42295e94da8b248
Alex <alex2324@cmis.ac.th>
2018-08-22 20:52:50 +0700

    Finished number 1

--- HelloWorld/Fundamentals1.java
+++ HelloWorld/Fundamentals1.java
@@ -15,24 +15,21 @@ public class Fundamentals1
         boolean f = true;
         char g = 'A';
         String h = "Byte data type is an 8-bit signed two's complement integer";
-        String i = "Byte can represent the amount of fingers a person has.";
-        String ae = "Byte can represent the amount of pencils a person has";
+        String i = "Byte can represent the amount of fingers a person has, the amount of pencils a person has, and the amount of toes a person has";
         String j = "Short data type is a 16-bit signed two's complement integer";
-        String k = "Short can represent the amount of sleep that I want (in seconds)";
-        String kd = "Short can represent 170 weeks in hours";
+        String k = "Short can represent the amount of sleep that I want (in seconds), the amount of pandas alive, and the amount of tissue I use in a month";
         String m = "Int data type is a 32-bit signed two's complement integer.";
-        String l = "Int can represent the amount of calories 15 people consumed in their lifetime";
-        String lm = "Int can represent the amount of seconds a person can live";
+        String l = "Int can represent the amount of calories 15 people consumed in their lifetime, the amount of seconds I live, amount of money a company can lose";
         String n = "Long data type is a 64-bit signed two's complement integer";
-        String o = "Long data can represent a light year in meters";
+        String o = "Long data can represent a light year in meters, Long can represent the amount of toilet paper rolls the world uses in a billion years,the diameter of Jupiter";
         String p = "Float data type is a single-precision 32-bit IEEE 754 floating point";
-        String q = "Float can be represented as 1 million light year in cenimeters";
+        String q = "Float can be represented as 1 million light year in cenimeters, the diameter of milky way, diameter of Andromeda Galaxy";
         String r = "Double data type is a double-precision 64-bit IEEE 754 floating point";
-        String t = "Double can be represented as one quintillion Jeff Bezos's net worth";
+        String t = "Double can be represented as one quintillion Jeff Bezos's net worth, the amount of time everyone in the world spends sleeping in their lifetime, the diameter of Kepler-39 ";
         String u = "Boolean data type represents one bit of information";
-        String v = "Boolean can be represented as am I a boy?";
+        String v = "Boolean can be represented as am I a boy?, Is it 6 pm?, and Is this color red?";
         String w = "Char data type is a single 16-bit Unicode character";
-        String x = "Char can represent the most expensive keyboard in Cambodian riel";
+        String x = "Char can represent the most expensive keyboard in Cambodian riel, amount of Bornean orangutan left in the world, and the amount of mosquitoes people catch in Thailand";
         
 
       
