2c72b3103b4c42bb93c86f6a39d307423fb8d4aa
Alex <alex2324@cmis.ac.th>
2018-08-31 12:00:08 +0700

    Got the top and bottom border

--- HelloWorld/Checkerboard.java
+++ HelloWorld/Checkerboard.java
@@ -6,23 +6,34 @@ public class Checkerboard {
         int in_2 = Scan.nextInt();
         System.out.println(checkerboard(in_1, in_2));
     }    
-    
+
     public static String checkerboard(int h, int w){
         String output = "";
         String mid = "";
-            for (int a = 0; a < h; a++){
-                if (a == 0){
+        String mid2 = "";
+        for (int a = 0; a <= h+1; a++){
+            if (a == 0){
                 for (int c = 0; c < w; c ++){
-                        mid += "-";
+                    mid += "-";
                 }
                 output += "+" + mid + "+";
-            }else{
-                for (int b = 0; b < w; b ++){
-                        output += ".";
-                }
-                output += "\n";
             }
+            else if(a == h +1){
+                for (int d = 0; d < w; d++){
+                    mid2 += "-";
+                }
+                output += "+" + mid2 + "+";
+            }
+            else if (a == 0){
+                
+            }
+            else{
+                for (int b = 0; b < w; b ++){
+                    output += ".";
+                }
+            }
+            output += "\n";
         }
-        return output;
+        return output;  
     }
 }
