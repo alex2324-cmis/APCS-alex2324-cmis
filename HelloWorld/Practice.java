febf7c3230aaba1b23d149185ed64974b38ec2f6
Alex <alex2324@cmis.ac.th>
2018-09-09 20:02:09 +0700

    Inside Checkerboard

---
+++ HelloWorld/Practice.java
@@ -0,0 +1,14 @@
+
+public class Practice
+{
+    {
+        for (int i = 10, j = 0; 
+        i > 0 && j != 3; 
+        i--, j = (int)(Math.random() * 10))
+        {
+            System.out.println(i * j);
+        }
+
+    }
+}
+
