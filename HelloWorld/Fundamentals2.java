e5e52d0523e35e409a8d7ca03734319860b01c4f
Alex <alex2324@cmis.ac.thgit config --global user.email alex2324@cmis.ac.thgit config --global user.email alex2324@cmis.ac.th>
2018-09-20 14:40:17 +0700

    Fundamentals 2

---
+++ HelloWorld/Fundamentals2.java
@@ -0,0 +1,15 @@
+public class Fundamentals2
+{
+    public static void main(String args[]){
+        int[] array = {1,2,4,6,8};
+        double[] arrayy = {1.4, 2.5,3.6,2.6,6.7};
+        String[] arrayyy = {"Hellow", "World", "Dog", "Moooo", "House"};
+        System.out.println(array [0]);
+        System.out.println(arrayy [2]);
+        System.out.println(arrayyy [4]);
+    }
+    //number 1
+    public static void printArray(int a, int b){
+    
+    }
+}
