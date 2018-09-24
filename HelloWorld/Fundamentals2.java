706640a16702f3d1718e4a0778819f3c06040908
Alex <alex2324@cmis.ac.th>
2018-09-24 09:55:06 +0700

    Fundamentals 2 #3

--- HelloWorld/Fundamentals2.java
+++ HelloWorld/Fundamentals2.java
@@ -9,7 +9,26 @@ public class Fundamentals2
         System.out.println(arrayyy [4]);
     }
     //number 1
-    public static void printArray(int a, int b){
-    
+    public static void printArray(int[] array, boolean skip){  
+        int x = 0;
+        int y = 0;
+        if(skip == true){
+            while(x < array.length){
+                System.out.println(array[x]);
+                x += 2;
+            }
+        }
+        else if(skip == false){
+            while (y < array.length){
+                System.out.println(array[y]);
+                y += 1;
+            }
+        }
+    }
+    //number 2
+    public static void lastItem(double[] array){
+        int x = 0;
+        int y = 0;
+        
     }
 }
