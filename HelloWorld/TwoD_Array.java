a3e6eca5c865be5c28813f6f4c76fb48c2151e99
Alex <alex2324@cmis.ac.th>
2018-10-24 14:35:16 +0700

    2darray

---
+++ HelloWorld/TwoD_Array.java
@@ -0,0 +1,13 @@
+public class TwoD_Array
+{
+    public static int[][] defaultArray(int h, int w){
+        int arry2d [][] = new int [h][w];
+        for(int x = 0; x < arry2d.length; x++){
+            for(int y = 0; y < arry2d[0].length; y++){
+                int out = arry2d[x][y];
+                
+            }
+        }
+        return arry2d;
+    }
+}
