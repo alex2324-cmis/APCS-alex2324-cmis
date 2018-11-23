5ee5806340e487b9d9045a5146df04a76e85fb4f
alex <alex2324@cmis.ac.th>
2018-11-23 09:45:52 +0700

    #11

--- HelloWorld/FundamentalsIII.java
+++ HelloWorld/FundamentalsIII.java
@@ -120,13 +120,57 @@ public class FundamentalsIII
         }
         return array;
     }
-    
+
     public static double[][] tilt(double[][] arr){
-        for(int row = 0; row < arr.length; row ++){
-            for(int col = 0; col < arr[0].length; col++){
-                arr[row][col] = arr[col][row];
+        double[][]output = new double [arr.length][arr[0].length];
+        for(int a = 0; a < arr.length; a ++){
+            for(int b = 0; b < arr[0].length; b++){
+                output[b][a] = arr[a][b];
             }
         }
-        return arr;
+        return output;
+    }
+
+    public static int[][] eleven(int[][] array, int width, int height){
+        int x = 0;
+        int y = 0;
+        int a = 0;
+        int z = 0;
+        int d = 0;
+        int p = 0;
+        for(int row = 0; row < array.length; row++){
+            for(int col1 = 0; col1 < array[0].length; col1++){
+                x += array[row][col1];
+            } 
+        }
+        for(int row = 1; row < array.length; row++){
+            for(int col2 = 0; col2 < array[0].length; col2++){
+                y += array[row][col2];
+            } 
+        }
+        for(int row = 2; row < array.length; row++){
+            for(int col3 = 0; col3 < array[0].length; col3++){
+                a += array[row][col3];
+            } 
+        }
+        for(int row = 0; row < array.length; row++){
+            for(int col1 = 0; col1 < array[0].length; col1++){
+                z += array[col1][row];
+            } 
+        }
+        for(int row = 1; row < array.length; row++){
+            for(int col2 = 0; col2 < array[0].length; col2++){
+                d += array[col2][row];
+            } 
+        }
+        for(int row = 2; row < array.length; row++){
+            for(int col3 = 0; col3 < array[0].length; col3++){
+                p += array[col3][row];
+            } 
+        }
+        //if (x > y && a && z && d && p){
+            
+        //}
+        return array;
     }
 }
